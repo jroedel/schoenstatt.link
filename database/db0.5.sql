53470cd83c6f446c57a6209f23cdb5e09db5447a
Jeff Roedel <3315281+jroedel@users.noreply.github.com>
2017-01-19 20:45:03 +0100

    Updated dependencies

--- database/db0.5.sql
+++ database/db0.5.sql
@@ -1,3 +1,4 @@
 
 
 ALTER TABLE `sch_persons` CHANGE `FirstName` `FirstName` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL, CHANGE `FirstNameWithoutAccents` `FirstNameWithoutAccents` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+ALTER TABLE `trans_phrases` ADD `origin_route` VARCHAR(255) NULL DEFAULT NULL AFTER `added_on`;
