84a9ab129700921f030060b243aff913cea9ef96
Jeff Roedel <3315281+jroedel@users.noreply.github.com>
2017-05-04 09:42:59 -0300

    Some basic changes

--- module/Schoenstatt/src/Schoenstatt/Service/AssociationFormFactory.php
+++ module/Schoenstatt/src/Schoenstatt/Service/AssociationFormFactory.php
@@ -40,6 +40,8 @@ class AssociationFormFactory implements FactoryInterface
 		$form->get('parent')->setValueOptions($associations);
 		$form->get('kind')->setValueOptions($kinds);
 		$form->get('country')->setValueOptions($countryNames);
+		$form->get('post1Country')->setValueOptions($countryNames);
+		$form->get('post2Country')->setValueOptions($countryNames);
 		return $form;
     }
 }
