ac69e36a80da24cc16fad31fe72e06a6b57336a2
Jeff Roedel <3315281+jroedel@users.noreply.github.com>
2017-12-06 11:20:06 -0300

    Make links to book show page

--- database/db2.2.sql
+++ database/db2.2.sql
@@ -1,3 +1,5 @@
+#submitted 2017-12-06
+
 UPDATE `lib_books` SET lang=NULL WHERE lang='';
 UPDATE `lib_books` SET lang='en' WHERE lang='eng';
 UPDATE `lib_books` SET lang='es|de|en' WHERE lang='es;de;en';
@@ -6,22 +8,5 @@ UPDATE `lib_books` SET lang='es' WHERE lang='ceb';
 UPDATE `lib_books` SET lang='de' WHERE lang='bs';
 UPDATE `lib_books` SET `author` = REPLACE(`author`, '; ', '|') WHERE (`author` LIKE '%; %');
 UPDATE `lib_books` SET `author` = REPLACE(`author`, ';', '|') WHERE (`author` LIKE '%;%');
-
-
-CREATE TABLE `lib_categories` (
-  `LibraryId` int(11) NOT NULL,
-  `CategoryName` varchar(500) NOT NULL,
-  `Abbreviation` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `lib_categories`
---
-ALTER TABLE `lib_categories`
-  ADD PRIMARY KEY (`LibraryId`,`CategoryName`);
 
 ALTER TABLE `lib_libraries` ADD `CallNumberPlaceholder` VARCHAR(50) NULL DEFAULT NULL AFTER `Description`;
