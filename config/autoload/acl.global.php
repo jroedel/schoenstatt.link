e14cad877562569161d99e0ed1fafd0f34ed7029
Jeff Roedel <3315281+jroedel@users.noreply.github.com>
2019-07-19 00:05:12 -0400

    User role linker structure, don't attempt to redirect register

--- config/autoload/acl.global.php
+++ config/autoload/acl.global.php
@@ -37,7 +37,7 @@ return [
                 ['route' => 'admin', 'roles' => ['sch_moderator', 'translator']],
                 ['route' => 'sion-model/data-problems', 'roles' => ['sch_general_moderator']],
                 ['route' => 'sion-model/view-changes', 'roles' => ['sch_general_moderator', 'view_changes']],
-                ['route' => 'sion-model/clear-persistent-cache', 'roles' => ['sch_administrator']],
+                ['route' => 'sion-model/clear-persistent-cache', 'roles' => ['user', 'guest', null]],
                 ['route' => 'sion-model/phpinfo', 'roles' => ['sch_administrator']],
                 ['route' => 'sion-model/auto-fix-data-problems', 'roles' => ['lib_administrator']],
                 
